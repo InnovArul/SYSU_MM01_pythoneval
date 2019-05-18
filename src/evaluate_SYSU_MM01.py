4731f17a67e08baa22a786354b287e4379f39696
arul <arul.csecit@ymail.com>
2019-05-18 19:24:29 +0530

    repliation of SYSU_MM01 matlab evaluation code

--- src/evaluate_SYSU_MM01.py
+++ src/evaluate_SYSU_MM01.py
@@ -1,5 +1,6 @@
 import os, sys, os.path as osp
 import gen_utils
+import numpy as np
 
 
 def evaluate(feature_dir, prefix, result_dir, settings):
@@ -22,49 +23,63 @@ def evaluate(feature_dir, prefix, result_dir, settings):
 
     for cam_index in all_cams:
         # read features
-        cam_feature_file = osp.join(feature_dir, (prefix + '_cam{}').format(cam_index))
-        features['cam' + str(cam_index)] = gen_utils.load_feature_file(cam_feature_file)
-    
+        cam_feature_file = osp.join(feature_dir, (prefix + "_cam{}").format(cam_index))
+        features["cam" + str(cam_index)] = gen_utils.load_feature_file(cam_feature_file)
+
     # perform testing
     print(list(features.keys()))
-    cam_id_locations = [1, 2, 2, 4, 5, 6]; # camera 2 and 3 are in the same location
+    cam_id_locations = [1, 2, 2, 4, 5, 6]
+    # camera 2 and 3 are in the same location
     mAPs = []
     cmcs = []
 
     for run_index in range(10):
-        X_gallery, Y_gallery, cam_gallery, X_probe, Y_probe, cam_probe = gen_utils.get_testing_set(features, cam_permutations,
-                                                                                            test_ids, run_index, cam_id_locations,
-                                                                                            gallery_cams, probe_cams, settings)
+        print("trial #{}".format(run_index))
+        X_gallery, Y_gallery, cam_gallery, X_probe, Y_probe, cam_probe = gen_utils.get_testing_set(
+            features,
+            cam_permutations,
+            test_ids,
+            run_index,
+            cam_id_locations,
+            gallery_cams,
+            probe_cams,
+            settings,
+        )
 
         # print(X_gallery.shape, Y_gallery.shape, cam_gallery.shape, X_probe.shape, Y_probe.shape, cam_probe.shape)
 
         dist = gen_utils.euclidean_dist(X_probe, X_gallery)
 
-        cmc = gen_utils.get_cmc_multi_cam(Y_gallery, cam_gallery, Y_probe, cam_probe, dist)
-        mAP = gen_utils.get_mAP_multi_cam(Y_gallery, cam_gallery, Y_probe, cam_probe, dist)
+        cmc = gen_utils.get_cmc_multi_cam(
+            Y_gallery, cam_gallery, Y_probe, cam_probe, dist
+        )
+        mAP = gen_utils.get_mAP_multi_cam(
+            Y_gallery, cam_gallery, Y_probe, cam_probe, dist
+        )
 
-        print('rank 1 5 10 20', cmc[0,4,9,19])
-        print('mAP', map)
+        print("rank 1 5 10 20", cmc[[0, 4, 9, 19]])
+        print("mAP", mAP)
         cmcs.append(cmc)
         mAPs.append(mAP)
-    
+
     # find mean mAP and cmc
-    cmcs = np.array(cmcs) # 10 x #gallery
-    mAPs = np.array(mAPs) # 10
+    cmcs = np.array(cmcs)  # 10 x #gallery
+    mAPs = np.array(mAPs)  # 10
     mean_cmc = np.mean(cmcs, axis=0)
     mean_mAP = np.mean(mAPs)
-    print('mean rank 1 5 10 20', mean_cmc[0,4,9,19])
-    print('mean mAP', mean_mAP)
-    
+    print("mean rank 1 5 10 20", mean_cmc[[0, 4, 9, 19]])
+    print("mean mAP", mean_mAP)
+
+
 if __name__ == "__main__":
     # feature_dir = "../../../scratch/sysu_mm01/deepzeropadding-14May2019-125214_deep-zero-padding/deep_zero_model#21"
     # prefix = "deep_zero_model#21"
-    feature_dir = './feature_original'
-    prefix = 'feat_deep_zero_padding'
+    feature_dir = "./feature_original"
+    prefix = "feat_deep_zero_padding"
     result_dir = "./result"
 
     # evaluation settings
     settings = {}
-    settings["mode"] = "indoor"  # indoor | all
-    settings["number_shot"] = 1  # 1 = single-shot | 10 = multi-shot
+    settings["mode"] = "all"  # indoor | all
+    settings["number_shot"] = 10  # 1 = single-shot | 10 = multi-shot
     evaluate(feature_dir, prefix, result_dir, settings)
--- src/gen_utils.py
+++ src/gen_utils.py
@@ -38,7 +38,7 @@ def get_cam_permutation_indices(all_cams):
     all_permutations = {}
 
     for cam_index in all_cams:
-        cam_permutations = mat_cam_permutations[cam_index-1][0].squeeze()
+        cam_permutations = mat_cam_permutations[cam_index - 1][0].squeeze()
         cam_name = "cam" + str(cam_index)
         if cam_name not in all_permutations:
             all_permutations[cam_name] = {}
@@ -77,9 +77,16 @@ def get_testing_set(
 ):
     # cam is indexed from 1 - 6
     # person indices are indexed using 0-based numbers
-    X_gallery_rgb, Y_gallery_rgb, cam_gallery_rgb, X_probe_IR, Y_probe_IR, cam_probe_IR = [], [], [], [], [], []
+    X_gallery_rgb, Y_gallery_rgb, cam_gallery_rgb, X_probe_IR, Y_probe_IR, cam_probe_IR = (
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+    )
 
-    number_shot = settings['number_shot']
+    number_shot = settings["number_shot"]
 
     # collect rgb images as gallery
     for cam_index in gallery_cams:
@@ -89,11 +96,12 @@ def get_testing_set(
         # for all the test ids, collect features
         for test_id in test_ids:
             current_id_features = current_cam_features[test_id]
-            
-            if np.any(np.array(current_id_features.shape) == 0): continue
+
+            if np.any(np.array(current_id_features.shape) == 0):
+                continue
             # assert (not np.any(np.array(current_id_features.shape) == 0)), 'test id feature count is 0'
 
-            # get the current permutation 
+            # get the current permutation
             current_permutation = cam_permutations[cam_name][test_id][run_index]
             current_permutation = current_permutation[:number_shot]
 
@@ -102,13 +110,15 @@ def get_testing_set(
             if len(X_gallery_rgb) == 0:
                 X_gallery_rgb = selected_features
             else:
-                X_gallery_rgb = np.concatenate((X_gallery_rgb, selected_features), axis=0)
+                X_gallery_rgb = np.concatenate(
+                    (X_gallery_rgb, selected_features), axis=0
+                )
 
-            Y_gallery_rgb += ([test_id] * number_shot)
-            cam_gallery_rgb += ([cam_id_locations[cam_index-1]] * number_shot)
+            Y_gallery_rgb += [test_id] * number_shot
+            cam_gallery_rgb += [cam_id_locations[cam_index - 1]] * number_shot
 
-    Y_gallery_rgb = np.array(Y_gallery_rgb)
-    cam_gallery_rgb = np.array(cam_gallery_rgb)
+    Y_gallery_rgb = np.array(Y_gallery_rgb, dtype=np.int)
+    cam_gallery_rgb = np.array(cam_gallery_rgb, dtype=np.int)
 
     # collect all the IR
     for cam_index in probe_cams:
@@ -118,22 +128,112 @@ def get_testing_set(
         # for all the test ids, collect features
         for test_id in test_ids:
             current_id_features = current_cam_features[test_id]
-            if np.any(np.array(current_id_features.shape) == 0): continue
+            if np.any(np.array(current_id_features.shape) == 0):
+                continue
             # assert len(current_id_features) != 0, 'test id feature count is 0'
 
             if len(X_probe_IR) == 0:
                 X_probe_IR = current_id_features
-                
+
             else:
                 X_probe_IR = np.concatenate((X_probe_IR, current_id_features), axis=0)
-            
-            Y_probe_IR += ([test_id] * len(current_id_features))
-            cam_probe_IR += ([cam_id_locations[cam_index-1]] * len(current_id_features))
 
-    Y_probe_IR = np.array(Y_probe_IR)
-    cam_probe_IR = np.array(cam_probe_IR)
-    
-    return X_gallery_rgb, Y_gallery_rgb, cam_gallery_rgb, X_probe_IR, Y_probe_IR, cam_probe_IR
+            Y_probe_IR += [test_id] * len(current_id_features)
+            cam_probe_IR += [cam_id_locations[cam_index - 1]] * len(current_id_features)
 
-def get_cmc_multi_cam(Y_gallery, Y_cam_gallery, Y_probe, Y_cam_probe, dist):
-    pass
+    Y_probe_IR = np.array(Y_probe_IR, dtype=np.int)
+    cam_probe_IR = np.array(cam_probe_IR, dtype=np.int)
+
+    return (
+        X_gallery_rgb,
+        Y_gallery_rgb,
+        cam_gallery_rgb,
+        X_probe_IR,
+        Y_probe_IR,
+        cam_probe_IR,
+    )
+
+
+def get_unique(array):
+    _, idx = np.unique(array, return_index=True)
+    return array[np.sort(idx)]
+
+
+def get_cmc_multi_cam(Y_gallery, cam_gallery, Y_probe, cam_probe, dist):
+    # dist = #probe x #gallery
+    num_probes, num_gallery = dist.shape
+    gallery_unique_count = get_unique(Y_gallery).shape[0]
+    match_counter = np.zeros((gallery_unique_count))
+
+    # sort the distance matrix
+    sorted_indices = np.argsort(dist, axis=-1)
+
+    Y_result = Y_gallery[sorted_indices]
+    cam_locations_result = cam_gallery[sorted_indices]
+
+    valid_probe_sample_count = 0
+
+    for probe_index in range(num_probes):
+        # remove gallery samples from the same camera of the probe
+        Y_result_i = Y_result[probe_index, :]
+        Y_result_i[cam_locations_result[probe_index, :] == cam_probe[probe_index]] = -1
+
+        # remove the -1 entries from the label result
+        # print(Y_result_i.shape)
+        Y_result_i = np.array([i for i in Y_result_i if i != -1])
+        # print(Y_result_i.shape)
+
+        # remove duplicated id in "stable" manner
+        Y_result_i_unique = get_unique(Y_result_i)
+        # print(Y_result_i_unique, gallery_unique_count)
+
+        # match for probe i
+        match_i = Y_result_i_unique == Y_probe[probe_index]
+
+        if np.sum(match_i) != 0:  # if there is true matching in gallery
+            valid_probe_sample_count += 1
+            match_counter += match_i
+
+    rankk = match_counter / valid_probe_sample_count
+    cmc = np.cumsum(rankk)
+    return cmc
+
+
+def get_mAP_multi_cam(Y_gallery, cam_gallery, Y_probe, cam_probe, dist):
+    # dist = #probe x #gallery
+    num_probes, num_gallery = dist.shape
+
+    # sort the distance matrix
+    sorted_indices = np.argsort(dist, axis=-1)
+
+    Y_result = Y_gallery[sorted_indices]
+    cam_locations_result = cam_gallery[sorted_indices]
+
+    valid_probe_sample_count = 0
+    avg_precision_sum = 0
+
+    for probe_index in range(num_probes):
+        # remove gallery samples from the same camera of the probe
+        Y_result_i = Y_result[probe_index, :]
+        Y_result_i[cam_locations_result[probe_index, :] == cam_probe[probe_index]] = -1
+
+        # remove the -1 entries from the label result
+        # print(Y_result_i.shape)
+        Y_result_i = np.array([i for i in Y_result_i if i != -1])
+        # print(Y_result_i.shape)
+
+        # match for probe i
+        match_i = Y_result_i == Y_probe[probe_index]
+        true_match_count = np.sum(match_i)
+
+        if true_match_count != 0:  # if there is true matching in gallery
+            valid_probe_sample_count += 1
+            true_match_rank = np.where(match_i)[0]
+
+            ap = np.mean(
+                np.array(range(1, true_match_count + 1)) / (true_match_rank + 1)
+            )
+            avg_precision_sum += ap
+
+    mAP = avg_precision_sum / valid_probe_sample_count
+    return mAP
